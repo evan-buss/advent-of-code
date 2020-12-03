e2b1e4159f0730033b91953f067dcd81aff27df0
Evan Buss <evan.buss28@gmail.com>
2020-12-02 19:35:01 -0500

    Day 2

---
+++ 2020/day2/day2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+var lines = (from line in File.ReadAllLines("input.txt")
+             select line.Trim()).ToList();
+
+void Part1()
+{
+    Console.WriteLine("Part 1: {0}", lines.Where(CheckLine).Count());
+
+    bool CheckLine(string line)
+    {
+        var parts = line.Split(" ");
+
+        var range = parts[0].Split("-").Select(int.Parse).ToArray();
+        var (min, max) = (range[0], range[1]);
+
+        var character = parts[1][0];
+        var password = parts[2];
+
+        var count = password.Where(x => x == character).Count();
+
+        return count >= min && count <= max;
+    }
+}
+
+void Part2()
+{
+    Console.WriteLine("Part 2: {0}", lines.Where(CheckLine).Count());
+
+    bool CheckLine(string line)
+    {
+        var parts = line.Split(" ");
+
+        var range = parts[0].Split("-").Select(int.Parse).ToArray();
+        var (pos1, pos2) = (range[0], range[1]);
+
+        var character = parts[1][0];
+        var password = parts[2];
+
+        var count = password.Where((c, i) => (i + 1 == pos1 || i + 1 == pos2) && password[i] == character).Count();
+        return count == 1;
+    }
+}
+
+Part1();
+Part2();
