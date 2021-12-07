94d268bba75b0f6d47da896eb446129dc88f8eb6
Evan Buss <evan.buss28@gmail.com>
2021-12-07 09:30:09 -0500

    Complete day 7.

---
+++ 2021/day7/Program.cs
@@ -0,0 +1,52 @@
+﻿var input = File.ReadAllText("input.txt").Split(",").Select(int.Parse).ToArray();
+
+Part1();
+Part2();
+
+void Part1()
+{
+  var min = input.Min();
+  var max = input.Max();
+
+  int minDistance = int.MaxValue;
+  for (int i = min; i < max; i++)
+  {
+    var total = 0;
+    foreach (var crab in input)
+    {
+      total += Math.Abs(crab - i);
+    }
+    minDistance = Math.Min(minDistance, total);
+  }
+
+  Console.WriteLine($"Part 1: {minDistance}");
+}
+
+void Part2()
+{
+  var min = input.Min();
+  var max = input.Max();
+
+  int minDistance = int.MaxValue;
+  for (int i = min; i < max; i++)
+  {
+    var total = 0;
+    foreach (var crab in input)
+    {
+      total += FuelCalc(Math.Abs(crab - i));
+    }
+    minDistance = Math.Min(minDistance, total);
+  }
+
+  Console.WriteLine($"Part 2: {minDistance}");
+}
+
+static int FuelCalc(int distance)
+{
+  var sum = 0;
+  for (int i = 1; i <= distance; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
