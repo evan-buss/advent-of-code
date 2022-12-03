9018931d7805f090da32b5fb1b53b3527acbc7b1
Evan Buss <evan.buss28@gmail.com>
2022-12-03 11:46:10 -0500

    day3 complete

---
+++ 2022/day3/Program.cs
@@ -0,0 +1,11 @@
+﻿var part1 = File.ReadLines("part1.txt")
+    .Select(x => x[..(x.Length / 2)].Intersect(x[(x.Length / 2)..]).First())
+    .Sum(c => char.IsUpper(c) ? c - 'A' + 27 : c - 'a' + 1);
+
+var part2 = File.ReadLines("part1.txt")
+    .Chunk(3)
+    .Select(x => x[0].Intersect(x[1]).Intersect(x[2]).First())
+    .Sum(c => char.IsUpper(c) ? c - 'A' + 27 : c - 'a' + 1);
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
