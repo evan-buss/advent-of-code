cc9c5779a5389d7343d06ee9f758a977425a39ad
Evan Buss <evan.buss28@gmail.com>
2022-12-06 18:03:59 -0500

    day6 complete

---
+++ 2022/day6/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadAllText("input.txt");
+
+Console.WriteLine($"Part 1: {SignalProcessor(input)}");
+Console.WriteLine($"Part 2: {SignalProcessor(input, 14)}");
+
+int SignalProcessor(string stream, int size = 4)
+{
+    for (var i = 0; i < stream.Length - size; i++)
+    {
+        if (new HashSet<char>(stream[i..(i + size)]).Count == size) return i + size;
+    }
+
+    return -1;
+}
