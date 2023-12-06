c29093600ea2edaace1952d89824d3baaafb0e9c
Evan Buss <evan.buss28@gmail.com>
2023-12-05 22:26:23 -0500

    day 4

--- 2023/Problems/Day3.cs
+++ 2023/Problems/Day3.cs
@@ -7,10 +7,10 @@ namespace Problems;
 [Day(3)]
 public class Day3 : IProblem
 {
-    public record Coord(int X, int Y);
+    private record Coord(int X, int Y);
 
     [DebuggerDisplay($"{{{nameof(Value)}}}")]
-    public class PartCode(int value)
+    private class PartCode(int value)
     {
         public int Value { get; } = value;
     };
@@ -19,10 +19,81 @@ public class Day3 : IProblem
     [PuzzleFile("day3.txt", Expected = 559667)]
     public int Part1(string[] input)
     {
+        var (partCodes, symbols) = ParseInput(input);
+
+        var sum = 0;
+        foreach (var (coord, _) in symbols)
+        {
+            var startX = Math.Max(0, coord.X - 1);
+            var endX = Math.Min(input[0].Length, coord.X + 1);
+            var startY = Math.Max(0, coord.Y - 1);
+            var endY = Math.Min(input.Length, coord.Y + 1);
+
+            var symbolCodes = new HashSet<PartCode>();
+            for (var y = startY; y <= endY; y++)
+            {
+                for (var x = startX; x <= endX; x++)
+                {
+                    var searching = new Coord(x, y);
+                    if (partCodes.TryGetValue(searching, out var code))
+                    {
+                        symbolCodes.Add(code);
+                    }
+                }
+            }
+
+            sum += symbolCodes.Sum(x => x.Value);
+            symbolCodes.Clear();
+        }
+
+        return sum;
+    }
+
+    [SampleFile("day3.sample.txt", 6756)]
+    [PuzzleFile("day3.txt", Expected = 86841457)]
+    public int Part2(string[] input)
+    {
+        var sum = 0;
+        var (partCodes, symbols) = ParseInput(input);
+        foreach (var (coord, _) in symbols.Where(x => x.Value == '*'))
+        {
+            var startX = Math.Max(0, coord.X - 1);
+            var endX = Math.Min(input[0].Length, coord.X + 1);
+            var startY = Math.Max(0, coord.Y - 1);
+            var endY = Math.Min(input.Length, coord.Y + 1);
+
+            var symbolCodes = new HashSet<PartCode>();
+            for (var y = startY; y <= endY; y++)
+            {
+                for (var x = startX; x <= endX; x++)
+                {
+                    var searching = new Coord(x, y);
+                    if (partCodes.TryGetValue(searching, out var code))
+                    {
+                        symbolCodes.Add(code);
+                    }
+                }
+            }
+
+            if (symbolCodes.Count == 2)
+            {
+                sum += symbolCodes.Aggregate(1, (i, code) => i * code.Value);
+            }
+
+            symbolCodes.Clear();
+        }
+
+        return sum;
+    }
+
+    private static (Dictionary<Coord, PartCode>, Dictionary<Coord, char>) ParseInput(
+        IReadOnlyList<string> input
+    )
+    {
         var partCodes = new Dictionary<Coord, PartCode>();
         var symbols = new Dictionary<Coord, char>();
 
-        for (var y = 0; y < input.Length; y++)
+        for (var y = 0; y < input.Count; y++)
         {
             var accum = new List<char>();
             var coords = new List<Coord>();
@@ -55,38 +126,6 @@ public class Day3 : IProblem
             }
         }
 
-        var sum = 0;
-        foreach (var (coord, _) in symbols)
-        {
-            var startX = Math.Max(0, coord.X - 1);
-            var endX = Math.Min(input[0].Length, coord.X + 1);
-            var startY = Math.Max(0, coord.Y - 1);
-            var endY = Math.Min(input.Length, coord.Y + 1);
-
-            var symbolCodes = new HashSet<PartCode>();
-            for (var y = startY; y <= endY; y++)
-            {
-                for (var x = startX; x <= endX; x++)
-                {
-                    var searching = new Coord(x, y);
-                    if (partCodes.TryGetValue(searching, out var code))
-                    {
-                        symbolCodes.Add(code);
-                    }
-                }
-            }
-
-            sum += symbolCodes.Sum(x => x.Value);
-            symbolCodes.Clear();
-        }
-
-        return sum;
-    }
-
-    [SampleFile("day3.sample.txt", 6756)]
-    [PuzzleFile("day3.txt")]
-    public int Part2(string[] input)
-    {
-        return 0;
+        return (partCodes, symbols);
     }
 }
---
+++ 2023/Problems/Day4.cs
@@ -0,0 +1,75 @@
+﻿using Problems.Attributes;
+
+namespace Problems;
+
+[Day(4)]
+public class Day4 : IProblem
+{
+    public record Game(int Card, int MatchCount, int Value)
+    {
+        public static Game Parse(string line)
+        {
+            var parts = line.Split(": ");
+            var card = int.Parse(parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+            var games = parts[1]
+                .Split(" | ")
+                .Select(
+                    set =>
+                        set.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToHashSet()
+                )
+                .ToArray();
+
+            games[1].IntersectWith(games[0]);
+            return new(card, games[1].Count, (int)Math.Pow(2, games[1].Count - 1));
+        }
+    }
+
+    [SampleFile("day4.sample.txt", 13)]
+    [PuzzleFile("day4.txt", Expected = 21558)]
+    public int Part1(string[] input)
+    {
+        return input.Select(Game.Parse).Sum(x => x.Value);
+    }
+
+    [SampleFile("day4.sample.txt", 30)]
+    [PuzzleFile("day4.txt", Expected = 10425665)]
+    public int Part2(string[] input)
+    {
+        var games = input.Select(Game.Parse).ToArray();
+
+        var cards = new int[games.Length];
+        Array.Fill(cards, 1);
+
+        foreach (var game in games)
+        {
+            for (var i = game.Card; i < game.Card + game.MatchCount; i++)
+            {
+                cards[i] += cards[game.Card - 1];
+            }
+        }
+
+        return cards.Sum();
+
+        // Naive (260ms)
+        // var games = input.Select(Game.Parse).ToList();
+        //
+        // var cards = new Stack<Game>(games);
+        //
+        // var sum = 0;
+        // while (cards.Count > 0)
+        // {
+        //     var game = cards.Pop();
+        //     var winning = game.MatchingNumbers;
+        //     for (var i = game.Card; i < game.Card + winning; i++)
+        //     {
+        //         cards.Push(games[i]);
+        //     }
+        //
+        //     sum++;
+        // }
+        //
+        // return sum;
+    }
+}
