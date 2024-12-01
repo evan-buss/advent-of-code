6cf3ae51fae74af26831a1167464008b3d394ac9
Evan Buss <evan.buss28@gmail.com>
2024-12-01 17:36:28 -0500

    use script files

---
+++ 2024/day01.fsx
@@ -0,0 +1,36 @@
+﻿open System
+open System.IO
+
+let file =
+    match fsi.CommandLineArgs with
+    | [| _; file |] -> file
+    | _ -> "sample.txt"
+
+let input file =
+    file
+    |> File.ReadAllLines
+    |> Array.map _.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun parts ->
+        match parts with
+        | [| l; r |] -> int l, int r
+        | _ -> failwithf $"Invalid input line format: %A{parts}")
+    |> Array.unzip
+    ||> (fun l r -> Array.sort l, Array.sort r)
+
+let part1 left right =
+    Array.zip left right |> Array.sumBy (fun (x, y) -> abs (y - x))
+
+let part2 left right =
+    let counts = right |> Array.countBy id |> Map
+
+    left
+    |> Array.sumBy (fun v ->
+        counts
+        |> Map.tryFind v
+        |> Option.map (fun count -> count * v) // Some v * count
+        |> Option.defaultValue 0) // None -> 0
+
+let puzzle = input file
+
+printfn $"Part 1: {puzzle ||> part1}"
+printfn $"Part 2: {puzzle ||> part2}"
---
+++ 2024/playground.fsx
@@ -0,0 +1,11 @@
+﻿type User = { Name: string; Age: int }
+
+let users = [
+    { Name = "Alice"; Age = 25 }
+    { Name = "Bob"; Age = 30 }
+    { Name = "Charlie"; Age = 35 }
+]
+
+let found = List.contains { Name = "Bob"; Age = 30 } users
+
+printfn "%A" found
