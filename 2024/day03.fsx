126c9d7c00ed0249da35f639d861bbed10733634
Evan Buss <evan.buss28@gmail.com>
2024-12-03 10:12:58 -0500

    cleanup

--- 2024/day03.fsx
+++ 2024/day03.fsx
@@ -1,15 +1,14 @@
 open System.IO
 open System.Text.RegularExpressions
 
-let file =
+let puzzleFile =
     fsi.CommandLineArgs |> Array.tryItem 1 |> Option.defaultValue "sample.txt"
 
-let line = file |> File.ReadAllLines |> String.concat ""
+let readLines file =
+    file |> File.ReadAllLines |> String.concat ""
 
-let part1 =
-    let parser = Regex(@"mul\((\d{1,3}),(\d{1,3})\)")
-
-    parser.Matches line
+let part1 line =
+    Regex(@"mul\((\d{1,3}),(\d{1,3})\)").Matches line
     |> Seq.map (fun m -> int m.Groups.[1].Value, int m.Groups.[2].Value)
     |> Seq.fold (fun acc (a, b) -> acc + a * b) 0
 
@@ -22,10 +21,8 @@ type Status =
     | On
     | Off
 
-let part2 =
-    let parser = Regex(@"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)")
-
-    parser.Matches line
+let part2 line =
+    Regex(@"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").Matches line
     |> Seq.map (fun v ->
         match v.Groups.[0].Value with
         | "do()" -> Do
@@ -36,13 +33,15 @@ let part2 =
             let (acc, status) = acc
 
             match v with
-            | Mul(a, b) when status = On -> (acc + a * b, status)
-            | Mul _ -> (acc, status) // Off don't do anything
+            | Do -> (acc, On)
             | Dont -> (acc, Off)
-            | Do -> (acc, On))
+            | Mul(a, b) when status = On -> (acc + a * b, status)
+            | Mul _ -> (acc, status)) // Off don't do anything
         (0, On)
     |> Seq.last
     |> fst
 
-printfn $"Part 1: %A{part1}"
-printfn $"Part 2: %A{part2}"
+let lines = puzzleFile |> readLines
+
+printfn $"Part 1: {lines |> part1}"
+printfn $"Part 2: {lines |> part2}"
