af834c80d07fc175d01831144f55a5e8cd5da73d
Evan Buss <evan.buss28@gmail.com>
2024-12-03 10:38:57 -0500

    tweaks

--- 2024/day03.fsx
+++ 2024/day03.fsx
@@ -4,13 +4,9 @@ open System.Text.RegularExpressions
 let puzzleFile =
     fsi.CommandLineArgs |> Array.tryItem 1 |> Option.defaultValue "sample.txt"
 
-let readLines file =
-    file |> File.ReadAllLines |> String.concat ""
-
 let part1 line =
     Regex(@"mul\((\d{1,3}),(\d{1,3})\)").Matches line
-    |> Seq.map (fun m -> int m.Groups.[1].Value, int m.Groups.[2].Value)
-    |> Seq.fold (fun acc (a, b) -> acc + a * b) 0
+    |> Seq.sumBy (fun m -> int m.Groups.[1].Value * int m.Groups.[2].Value)
 
 type Command =
     | Do
@@ -28,20 +24,19 @@ let part2 line =
         | "do()" -> Do
         | "don't()" -> Dont
         | _ -> Mul(int v.Groups.[1].Value, int v.Groups.[2].Value))
-    |> Seq.scan
+    |> Seq.fold
         (fun acc v ->
-            let (acc, status) = acc
+            let acc, status = acc
 
             match v with
             | Do -> (acc, On)
             | Dont -> (acc, Off)
             | Mul(a, b) when status = On -> (acc + a * b, status)
-            | Mul _ -> (acc, status)) // Off don't do anything
+            | Mul _ -> (acc, status))
         (0, On)
-    |> Seq.last
     |> fst
 
-let lines = puzzleFile |> readLines
+let line = puzzleFile |> File.ReadAllText
 
-printfn $"Part 1: {lines |> part1}"
-printfn $"Part 2: {lines |> part2}"
+printfn $"Part 1: {line |> part1}"
+printfn $"Part 2: {line |> part2}"
