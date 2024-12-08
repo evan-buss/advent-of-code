8f121b0ae5894337b5f36eedd4b67ddd7bfa2217
Evan Buss <evan.buss28@gmail.com>
2024-12-08 18:40:52 -0500

    day5: simplify

--- 2024/day05.fsx
+++ 2024/day05.fsx
@@ -1,82 +1,42 @@
 let puzzleFile =
     fsi.CommandLineArgs |> Array.tryItem 1 |> Option.defaultValue "sample.txt"
 
-let parsePuzzle puzzleFile =
-    let parts =
-        System.IO.File.ReadAllLines puzzleFile
-        |> Array.partition (fun line -> line.Contains("|"))
+let parts =
+    System.IO.File.ReadAllLines puzzleFile
+    |> Array.partition (fun line -> line.Contains("|"))
 
-    let rules =
-        parts
-        |> fst
-        |> Array.map (fun line ->
-            let parts = line.Split("|")
-            (int parts.[0], int parts.[1]))
-        |> Array.fold
-            (fun acc (before, after) ->
-                match acc |> Map.tryFind before with
-                | Some(set) -> acc.Add(before, (Set.add after set))
-                | None -> acc.Add(before, Set.ofArray [| after |]))
-            Map.empty<int, Set<int>>
+let rules =
+    parts
+    |> fst
+    |> Array.map (fun line ->
+        let parts = line.Split("|")
+        (int parts[0], int parts[1]))
+    |> Set.ofArray
 
-    let pages =
-        parts
-        |> snd
-        |> Array.skip (1)
-        |> Array.map (fun line -> line.Split(",") |> Array.map int)
+let pages =
+    parts
+    |> snd
+    |> Array.skip (1)
+    |> Array.map (fun line -> line.Split(",") |> Array.map int)
 
-    rules, pages
+let comparer a b =
+    if Set.contains (a, b) rules then -1
+    else if Set.contains (b, a) rules then 1
+    else 0
 
-let validPage rules page acc =
-    match rules |> Map.tryFind page with
-    | Some(s) -> not (Set.exists (fun x -> Seq.contains x acc) s)
-    | None -> true
+let isValid instructions =
+    instructions |> Seq.pairwise |> Seq.forall (fun (a, b) -> comparer a b < 0)
 
-let validInstructions rules instructions =
-    let output =
-        instructions
-        |> Array.mapi (fun i page ->
-            match validPage rules page instructions[..i] with
-            | true -> Some(page)
-            | false -> None)
-        |> Array.choose id
+let middleElement (arr: int array) = arr[arr.Length / 2]
 
-    output.Length = instructions.Length
+let part1 = pages |> Array.filter isValid |> Array.sumBy (middleElement)
 
-let part1 () =
-    let rules, pages = parsePuzzle puzzleFile
-
+let part2 =
     pages
-    |> Array.filter (validInstructions rules)
-    |> Array.sumBy (fun x -> x.[x.Length / 2])
-
-let part2 () =
-    let rules, pages = parsePuzzle puzzleFile
-
-    let ruleSorter ruleSet (left: int) (right: int) =
-        let leftRules = Map.tryFind left ruleSet
-        let rightRules = Map.tryFind right ruleSet
-
-        match leftRules, rightRules with
-        | Some(leftSet), Some(rightSet) ->
-            if rightSet |> Set.contains left then -1
-            elif leftSet |> Set.contains right then 1
-            else 0
-        | Some(rules), None ->
-            match Set.contains right rules with
-            | true -> 1
-            | false -> 0
-        | None, Some(rules) ->
-            match Set.contains left rules with
-            | true -> -1
-            | false -> 0
-        | None, None -> 0
-
-    pages
-    |> Array.filter (not << validInstructions rules)
-    |> Array.map (Array.sortWith (ruleSorter rules))
-    |> Array.sumBy (fun x -> x.[x.Length / 2])
+    |> Array.filter (isValid >> not)
+    |> Array.map (Array.sortWith comparer)
+    |> Array.sumBy (middleElement)
 
 
-printfn $"Part 1: {() |> part1}"
-printfn $"Part 2: {() |> part2}"
+printfn $"Part 1: {part1}"
+printfn $"Part 2: {part2}"
