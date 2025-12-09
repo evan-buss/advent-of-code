d47d6c1c371b99770a0d2e36e225b13ea7f7ca55
Evan Buss <evan.buss28@gmail.com>
2025-12-09 12:26:39 -0500

    day3

--- 2025/day3.cs
+++ 2025/day3.cs
@@ -1,63 +1,40 @@
-using System.Collections.Immutable;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-
 var file = args switch
 {
     [var path] => path,
     _ => Console.ReadLine() ?? throw new ArgumentException("No file path provided")
 };
 
-var lines = File.ReadLines(file);
+var banks = File.ReadAllLines(file);
 
-// Console.WriteLine($"Part 1: {Parse(lines).Sum(bank => Solve(bank, 2))}");
-Console.WriteLine($"Part 2: {Parse(lines).Sum(bank => Solve(bank, 12))}");
+Console.WriteLine($"Part 1: {banks.Sum(bank => Solve(bank, 2))}");
+Console.WriteLine($"Part 2: {banks.Sum(bank => Solve(bank, 12))}");
 
-var Part1 = (int[] bank) => Solve(bank, 2);
-var Part2 = (int[] bank) => Solve(bank, 12);
-
-static long Solve(int[] bank, int limit)
+static long Solve(ReadOnlySpan<char> bank, int targetLength)
 {
-    var bankSize = bank.Length;
-    var cells = new int[bankSize];
-    int found = 0;
-    int start = 0;
+    var result = 0L;
+    var start = 0;
 
-    for (int target = 9; target >= 1 && found < limit; target--)
+    for (var end = bank.Length - targetLength; end < bank.Length; end++)
     {
-        for (int i = start; i < bank.Length; i++)
+        var maxIndex = IndexOfMaxValue(bank, start, end);
+        result = (result * 10) + (bank[maxIndex] - '0');
+        start = maxIndex + 1;
+    }
+
+    return result;
+
+    static int IndexOfMaxValue(ReadOnlySpan<char> bank, int start, int end)
+    {
+        var maxValue = char.MinValue;
+        var maxIndex = -1;
+        for (var i = start; i <= end; i++)
         {
-            if (bank[i] == target)
+            if (bank[i] > maxValue) // first largest wins
             {
-                cells[i] = target;
-                found++;
-
-                if (i != bankSize - found) // not last
-                {
-                    start = Math.Min(i, i + 1);
-                }
-            }
-
-            if (found == limit)
-            {
-                break;
+                maxValue = bank[i];
+                maxIndex = i;
             }
         }
+        return maxIndex;
     }
-
-    var sb = new StringBuilder();
-    for (int i = 0; i < cells.Length; i++)
-    {
-        if (cells[i] != 0)
-            sb.Append(cells[i]);
-    }
-
-    Console.WriteLine(sb.ToString());
-
-    return long.Parse(sb.ToString());
-}
-
-static IEnumerable<int[]> Parse(IEnumerable<string> lines)
-{
-    return lines.Select(row => row.Select(c => c - '0').ToArray());
 }
